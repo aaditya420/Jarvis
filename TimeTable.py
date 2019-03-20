6cd7915fa54d6d9a14e13109b2e3eca13ccd0278
LycurgusOrion <gamer.forever.untill.death@gmail.com>
2019-03-20 23:44:45 +0530

    WhatsApp PA Initial Commit

---
+++ TimeTable.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import six
+import datetime
+
+from selenium import webdriver
+
+
+CLASS_ID = 4
+BATCH_ID = "CS 1"
+
+def get_time_table():
+
+    try:
+        driver = webdriver.Firefox()
+    except:
+        driver = webdriver.Chrome()
+
+    url = "http://www.ncuindia.edu/timetable/cse2019.html"
+    driver.get(url)
+
+    headers = driver.find_elements_by_tag_name("h1")
+
+    headers = list(map(lambda x: x.text, headers))
+
+    table = driver.find_elements_by_tag_name("table")[CLASS_ID]
+
+    tbody = table.find_element_by_tag_name("tbody")
+    
+    i, j, k = 0, 0, 0
+    days = {}
+    for row in tbody.find_elements_by_tag_name("tr"):
+        lr = []
+        j = 0
+        for col in row.find_elements_by_tag_name("td"):
+            if (j > 0) and (i > 0):
+                if ((i % 2) is not 0):
+                    lr.append(col.text)
+            j += 1
+        lr = list(map(lambda a: (a[:3]+a[4:]) if (len(a) == 5) else list(a), list(filter(lambda z: (len(z) > 2) or ((len(z) > 5) and (z[3] == BATCH_ID)), list(map(lambda x: list(filter(lambda y: (len(y) > 0) and (y is not " "), x.split("\n"))), lr))))))
+        if len(lr) > 0:
+            days[k] = {sub[3] : str(sub[0] + " in " + sub[2]) for sub in lr}
+            k += 1
+        i += 1
+
+    driver.close()
+
+    df = pd.DataFrame(days)
+
+    df = df.T
+
+    cols = df.columns.tolist()
+    cols = cols[-2:] + cols[:-2]
+    df = df[cols]
+
+    df.fillna("BREAK", inplace=True)
+
+    index = ["MON", "TUE", "WED", "THU", "FRI"]
+
+    df.index = index
+    df = df.reset_index()
+    df.rename(columns={"index" : "Days"}, inplace=True)
+
+    df.to_csv("TimeTable.csv", index=False)
+
+    return df
+
+
+def render_mpl_table(data, col_width=3.0, row_height=3.5, font_size=32,
+                     header_color='#40466e', row_colors=['#f5f5f5', 'w'], edge_color='black',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in  six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax
+
+# render_mpl_table(df, header_columns=1, col_width=7).get_figure().savefig("TimeTableG.png", dpi=200)
+
+def fetch_next_room_number(df, next_rn=True):
+    
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+    currentDate = datetime.datetime.now()
+    today = currentDate.strftime("%a")
+
+    if today not in days:
+        return "Its A Holiday Mate!"
+    
+    slots = df.columns.tolist()[1:]
+    slots = list(map(lambda x: x.split(" - "), slots))
+    slots = [j for sub in slots for j in sub]
+
+    slots_obj = []
+    pos = 0
+
+    for slot in slots:
+        slots_obj.append(currentDate.replace(hour=int(slot.split(":")[0]), minute=int(slot.split(":")[1][:2])))
+    
+    if (currentDate > slots_obj[-1]):
+        today = days[(days.index(today) + 1) % 5]
+    
+    if not next_rn:
+        if ((currentDate < slots_obj[0]) or (currentDate > slots_obj[-1])):
+            return "You've no class right now!"
+        
+    for i in range(len(slots_obj)-1):
+        if ((currentDate > slots_obj[i]) and (currentDate < slots_obj[i+1])):
+            if next_rn:
+                pos = i+2
+            else:
+                pos = i
+            break
+
+    x = df.columns.tolist()[1:]
+    x = list(filter(lambda x: slots[pos] in x, x))[-1]
+    y = " ".join(str(df[df["Days"] == today.upper()][x]).split("\n")[0].split()[1:])
+    
+    if next_rn:
+        return "You're next class is " + y
+    else:
+        return "You're class right now is " + y
+
+
---
+++ app.py
@@ -0,0 +1,123 @@
+import os
+import TimeTable
+import pandas as pd
+import random as rd
+
+from flask import Flask, render_template, request
+from requests_html import HTMLSession
+from twilio.rest import Client
+
+
+TWILIO_ACCOUNT_SID="ACdb9304b0c1a9736432c77d3ce495e3f5"
+TWILIO_AUTH_TOKEN="f5db2c220080ad842bd8539b41960049"
+
+WHATSAPP_FROM_NUMBER="whatsapp:+14155238886"
+
+MSG_BODY = ""
+
+app = Flask(__name__)
+
+# Helper function to send WhatsApp messages
+def send_message(to, body):
+    # Initialize client
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    client.messages.create(
+        body=body,
+        from_=WHATSAPP_FROM_NUMBER,
+        to=to
+    )
+
+# Helper function to get an emoji's description
+def get_emojipedia_description(character):
+    # Get the Emojipedia page for this emoji
+    session = HTMLSession()
+    response = session.get('https://emojipedia.org/' + character)
+
+    # If we didn't find an emoji, say so
+    if not response.ok:
+        return "Hmm - I couldn't find that emoji. Try sending me a single emoji ☝️"
+
+    # Extract the title and description using Requests-HTML and format it a bit
+    title = response.html.find('h1', first=True).text
+    description = response.html.find('.description', first=True)
+    description = '\n\n'.join(description.text.splitlines()[:-1])
+
+    # And template it
+    return render_template(
+        'response.txt',
+        title=title,
+        description=description,
+        url=response.url
+    )
+
+@app.route('/', methods=['GET', 'POST'])
+def receive_message():
+    # Get the querie
+    querie = request.values.get('Body')
+
+    send_message(to=request.values['From'], body=parse_msg(querie))
+
+    return ('', 204)
+
+
+def Main():
+
+    try:
+        df = pd.read_csv("TimeTable.csv")
+        render_mpl_table(df, header_columns=1, col_width=7).get_figure().savefig("TimeTableG.png", dpi=200)
+    except:
+        TimeTable.get_time_table()
+
+
+def parse_msg(ql):
+
+    greetings = [
+        "Hi there! How's it going? I am JARVIS, How may I help you?",
+        "Hola Amigo! How can I be of help?",
+        "Heya! What's Up? How can I help ya?",
+        "Good Day mate! How can I be of any help to ya?!"
+    ]
+
+    see_offs = [
+        "Have a good day!",
+        "Pleasure helping you!",
+        "Have a good one mate!",
+        "Adios b*tch!",
+        "Sayonara!"
+    ]
+
+    errors = [
+        "I am sorry I quiet did not get that, please rephrase?",
+        "Pardon me?",
+        "I did not understand that, sorry!",
+        "I am sorry can you please be more specific?",
+        "B*tch I don't do that sh*t no more!",
+        "Are you okay? Coz this is something I cannot do!"
+    ]
+
+    ql = ql.lower()
+
+    if ("hi" in ql) or ("hello" in ql) or ("good morning" in ql) or ("hola" in ql) or ("hey" in ql):
+        resp = rd.choice(greetings)
+    
+    elif ("thank" in  ql) or ("bye" in ql):
+        resp = rd.choice(see_offs)
+
+    elif ("next" in ql) and ("class" in ql):
+        resp = TimeTable.fetch_next_room_number(pd.read_csv("TimeTable.csv"))
+    
+    elif (("current" in ql) or ("now" in ql) or ("right" in ql)) and ("class" in ql):
+        resp = TimeTable.fetch_next_room_number(pd.read_csv("TimeTable.csv"), False)
+    
+    elif (len(ql) == 1) and (ord(ql) > 200):
+        resp = get_emojipedia_description(ql)
+    
+    else:
+        resp = rd.choice(errors)
+
+    return resp
+
+
+if __name__ == "__main__":
+    Main()
+    # app.run(debug=True)
